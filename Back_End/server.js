604d0b9202e0cb5e7ef2d76a1c5922fc73ad1c7d
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-05-03 22:08:02 -0700

    chore(deploy_schemas.sql): add comments
    Add lecture note comments

--- Back_End/server.js
+++ Back_End/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const helmet = require('helmet');
 const cors = require('cors');
 const knex = require('knex');
-const morgan = require('morgan')
-
+const morgan = require('morgan');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
@@ -22,17 +22,36 @@ const db = knex({
 
 const app = express();
 
-app.use(morgan('combined'))
-app.use(cors())
+// Enable logging
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+// Enable pre-flight across-the-board
+app.options('*', cors()); // include before all other routes
+// Enable CORS
+var corsOptions = {
+  origin: ["http://localhost:3000", "http://localhost:3001"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}
+
+// Sets Referrer-Policy
+app.use(
+  helmet.referrerPolicy({
+    policy: "unsafe-url"
+  })
+);
+
+// CRUD API Routes
+app.get('/', cors(corsOptions), (req, res)=> { res.send(db.users) });
+app.post('/signin', cors(corsOptions), signin.handleSignin(db, bcrypt));
+app.post('/register', cors(corsOptions), (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.get('/profile/:id', cors(corsOptions), (req, res) => { profile.handleProfileGet(req, res, db)});
+app.put('/image', cors(corsOptions), (req, res) => { image.handleImage(req, res, db)});
+app.post('/imageurl', cors(corsOptions), (req, res) => { image.handleApiCall(req, res)});
 
 app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
-})
+  console.log('CORS-enabled API web server running on port 3000');
+});
