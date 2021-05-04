ea52b3732686fab2c44db5fc9b1e25f73cf1ee73
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-05-03 22:09:30 -0700

    chore(deploy_schemas.sql): add comments
    Add lecture note comments

--- Back_End/postgres/deploy_schemas.sql
+++ Back_End/postgres/deploy_schemas.sql
@@ -1,3 +1,5 @@
 -- Deploy fresh database tables
+-- \i executes scripts
+-- We are essentially running the SQL commands in each file to generate tables for the Docker container
 \i '/docker-entrypoint-initdb.d/tables/users.sql'
 \i '/docker-entrypoint-initdb.d/tables/login.sql'
