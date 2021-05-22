24b5c04a96f275b20a1595a02426a50bc965ef86
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-05-22 16:39:11 -0700

    feat(server.js): add authorization middleware to API routes and refactor cors
    add auth.requireAuth in the '/profile/:id', '/image', and '/imageurl' routes. the application
    wouldn't work with `cors(corsOptions), auth.requireAuth` in each api route, so refactored
    corsoptions by moving it outside of each individual route and into the application level.

--- Back_End/server.js
+++ Back_End/server.js
@@ -9,6 +9,7 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const auth = require('./middleware/authorization');
 
 const db = knex({
   client: 'pg',
@@ -27,15 +28,26 @@ const app = express();
 app.use(bodyParser.json());
 
 // Enable pre-flight across-the-board
-app.options('*', cors()); // include before all other routes
+// app.options('*', cors()); // include before all other routes
 // Enable CORS
-var corsOptions = {
-  origin: ["http://localhost:3000", "http://localhost:3001"],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204
+// var corsOptions = {
+//   origin: ["http://localhost:3000", "http://localhost:3001"],
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204
+// }
+const whitelist = ['http://localhost:3001']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
 }
+app.use(cors(corsOptions))
 
 // Sets Referrer-Policy
 // app.use(
@@ -45,13 +57,13 @@ var corsOptions = {
 // );
 
 // CRUD API Routes
-app.get('/', cors(corsOptions), (req, res)=> { res.send(db.users) });
-app.post('/signin', cors(corsOptions), signin.authentication(db, bcrypt));
-app.post('/register', cors(corsOptions), (req, res) => { register.handleRegister(req, res, db, bcrypt) });
-app.get('/profile/:id', cors(corsOptions), (req, res) => { profile.handleProfileGet(req, res, db)});
-app.post('/profile/:id', cors(corsOptions), (req, res) => { profile.handleProfileUpdate(req, res, db)});
-app.put('/image', cors(corsOptions), (req, res) => { image.handleImage(req, res, db)});
-app.post('/imageurl', cors(corsOptions), (req, res) => { image.handleApiCall(req, res)});
+app.get('/', (req, res)=> { res.send(db.users) });
+app.post('/signin', signin.authentication(db, bcrypt));
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)});
+app.post('/profile/:id',auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)});
+app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db)});
+app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)});
 
 app.listen(3000, ()=> {
   console.log('CORS-enabled API web server running on port 3000');
