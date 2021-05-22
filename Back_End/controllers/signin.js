abbc61cd36e24c0a9acead66b000531e30878dfb
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-05-22 16:43:16 -0700

    feat(signin.js): create json web token when user signs in
    import jsonwebtoken and redis libraries. configure redis client. create getAuthTokenID(req, res)
    function, setToken(tokenKey,idValue) function, createSession(user) function, and signToken(email)
    function. add error handling in authentication(db, bcrypt) function.

--- Back_End/controllers/signin.js
+++ Back_End/controllers/signin.js
@@ -1,3 +1,8 @@
+const { json } = require("body-parser");
+const jwt = require('jsonwebtoken');
+const redis = require("redis");
+const redisClient = redis.createClient(process.env.REDIS_URL);
+
 const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -10,7 +15,7 @@ const handleSignin = (db, bcrypt, req, res) => {
       if (isValid) {
         return db.select('*').from('users')
           .where('email', '=', email)
-          .then(user => {user[0]})
+          .then(user => user[0])
           .catch(err => Promise.reject(`unable to get user: ${err}`))
       } else {
         Promise.reject('wrong credentials')
@@ -19,16 +24,45 @@ const handleSignin = (db, bcrypt, req, res) => {
     .catch(err => Promise.reject(`wrong credentials: ${err}`))
 }
 
-const getAuthTokenID = () => {
-  console.log('JWT Token goes here')
+const getAuthTokenID = (req, res) => {
+  const { authorization } = req.headers;
+  console.log(authorization);
+  return redisClient.get(authorization, (err, reply) => {
+    if (err || !reply) {
+      return res.status(400).json(`Unauthorized: problem with getAuthTokenID: error: ${err}`);
+    }
+    return res.json({id: reply});
+  });
+}
+
+const setToken = (tokenKey,idValue) => {
+  return Promise.resolve(redisClient.set(tokenKey, idValue));
+}
+
+const createSession = (user) => {
+  const {email, id} = user;
+  const token = signToken(email);
+  return setToken(token, id)
+    .then(() => { 
+      return  {success: 'true', userId: id, token: token }
+    })
+    .catch(console.log);
+}
+
+const signToken = (email) => {
+  const jwtPayload = { email }
+  return jwt.sign(jwtPayload, process.env.JWT, { expiresIn: '2 days' });
 }
 
 const authentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers;
-  authorization ? getAuthTokenID() : 
-    handleSignin(req, res, db, bcrypt)
-      .then(data => res.json(data))
-      .catch(err => res.status(400).json(err))
+  return authorization ? getAuthTokenID(req, res) : 
+    handleSignin(db, bcrypt, req, res)
+      .then(data => {
+        return data.id && data.email ? createSession(data) : Promise.reject('user id and/or email does not exist') 
+      })
+      .then(session => res.json(session))
+      .catch(err => res.status(400).json(err));
 }
 
 module.exports = {
