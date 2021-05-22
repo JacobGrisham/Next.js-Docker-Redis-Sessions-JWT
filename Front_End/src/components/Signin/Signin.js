475b128fcbb7f535b2ed049daf0840c534490e22
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-05-22 16:16:43 -0700

    feat(signin.js): enable automatic saving of jsonwebtoken when user signs in
    add the saveAuthTokenInSession(token) function to save the token in localStorage. add a fetch
    request to the home page of the user at `http://localhost:3000/profile/id` in the onSubmitSignIn()
    function using the saved token.

--- Front_End/src/components/Signin/Signin.js
+++ Front_End/src/components/Signin/Signin.js
@@ -17,6 +17,11 @@ class Signin extends React.Component {
     this.setState({signInPassword: event.target.value})
   }
 
+  saveAuthTokenInSession = (token) => {
+    window.sessionStorage.setItem('token', token);
+    // use localStorage if you want to open a new tab and still be signed in
+  }
+
   onSubmitSignIn = () => {
     fetch('http://localhost:3000/signin', {
       method: 'post',
@@ -27,10 +32,23 @@ class Signin extends React.Component {
       })
     })
       .then(response => response.json())
-      .then(user => {
-        if (user.id) {
-          this.props.loadUser(user)
-          this.props.onRouteChange('home');
+      .then(data => {
+        if (data.userId && data.success === 'true') {
+          this.saveAuthTokenInSession(data.token);
+          fetch(`http://localhost:3000/profile/${data.userId}`, {
+            method: 'get',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': data.token
+            }
+          })
+          .then(resp => resp.json())
+          .then(user => {
+            if (user && user.email) {
+              this.props.loadUser(user);
+              this.props.onRouteChange('home');
+            }
+          })
         }
       })
   }
